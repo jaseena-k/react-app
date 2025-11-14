9cab286604298aca72f3753b01ec7a161cc781a7
jaseena-k <jaseenajas596@gmail.com>
2025-11-14 09:19:18 +0000

    current year updated

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -4,12 +4,13 @@ import './index.css'
 import App from './App.jsx'
 
 const name = "Jaseena"
-const num = 8
+const currentDate = new Date() ;
+const year =currentDate.getFullYear()
+console.log(currentDate)
+
 createRoot(document.getElementById('root')).render(
 <div>
-    <h1>Hi {name}</h1>
-    <p>Your lucky number is {num}</p>
-    <p>your lucky number is {5+7}</p>
-    <p>Your lucky number is {Math.floor(Math.random()*10)}</p>
+    <h1>created by{name}</h1>
+    <p>copyright{year}</p>
   </div>
 )
