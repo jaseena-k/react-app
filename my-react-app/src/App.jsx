03ae40e50574b5577e3dd1f1c40ccacbd25a2c75
jaseena-k <jaseenajas596@gmail.com>
2025-11-20 06:44:22 +0000

    props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
+import contacts from "./contacts"
+import Card from "./components/Card";
 
 function App(){
 
   return (
     <div>
-      <Header/>
-      <Note/>
-      <Footer/>
+      <h1 className = "heading">My Contacts</h1>
+      <Card
+      name = {contacts[0].name}
+      img ={contacts[0].imgURL}
+      tel ={contacts[0].phone}
+      email ={contacts[0].email}
+      />
+            <Card
+      name = {contacts[1].name}
+      img ={contacts[1].imgURL}
+      tel ={contacts[1].phone}
+      email ={contacts[1].email}
+      />
+            <Card
+      name = {contacts[2].name}
+      img ={contacts[2].imgURL}
+      tel ={contacts[2].phone}
+      email ={contacts[2].email}
+      />
+
     </div>
+    
+   
   )
 }
 
---
+++ my-react-app/src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Sid sriram",
+    imgURL:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTobtUjuD-DZMzeulNM__Y_2o-Jb9lqczR2DKcz-kElSIbjUqTziTZuyKYqWfDxiaiICCDVa6HsaTsXMKYJBnPGq6ZV7U2Sa_BwHBerMEcE&s=10",
+    phone: "+123 456 789",
+    email: "sid@sriram.com"
+  },
+  {
+    name: "Arijith Singn",
+    imgURL:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn9ZPEcKsW4GhU4EOmT9o3663B_P1aZTW2w1PH_fovOx8hH6YaksIBEu9QxaQdvdoTS7J6ZtJHlEmLKrBVGSRM4PmFNJ8MHMarbCpUr1lvYw&s=10",
+    phone: "+987 654 321",
+    email: "arijith@.com"
+  },
+  {
+    name: "Darshan Raval",
+    imgURL:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMa75Qy68ZiuHUSi9sqSuyuSkgVqxIA2vKFF7CFwVSzvNFtLnmEGxlApYq7tE4dRBzMKLqJ6uGWt5bLZpFw85hPP8c-ECBsVluosfhC29u5Q&s=10",
+    phone: "+918 372 574",
+    email: "gmaildarshan.com"
+  }
+];
+
+
+export default contacts
