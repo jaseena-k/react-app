ec496e4d35371f5a99711f300c830e8b2fcd731a
jaseena-k <jaseenajas596@gmail.com>
2025-11-25 09:02:36 +0000

    ternary and && operators

---
+++ react-basics/basic-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Form from "./Form";
+
+
+var userIsRegistered = false
+
+function App(){
+
+  return (
+    <div className="container">
+      <Form isRegistered ={userIsRegistered}/>
+      
+    </div>
+  )
+}
+
+export default App
